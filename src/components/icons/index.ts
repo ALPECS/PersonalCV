07c17752fa031096f838b1556a2d46e2ae25c589
Aldo Perez <aldo651371@gmail.com>
2024-01-14 20:26:42 -0800

    Initial commit to update small information

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,5 +1,5 @@
 import { GitHubIcon } from "./GitHubIcon";
 import { LinkedInIcon } from "./LinkedInIcon";
-import { XIcon } from "./XIcon";
+import { InstaIcon } from "./InstaIcon";
 
-export { GitHubIcon, LinkedInIcon, XIcon };
+export { GitHubIcon, LinkedInIcon, InstaIcon };
